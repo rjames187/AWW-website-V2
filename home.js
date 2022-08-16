f06216684c3cf97388dc982a960eaa619b28a00d
rjames187 <rory.james2021@gmail.com>
2022-08-15 21:36:15 -0500

    add responsive hamburger menu

--- home.js
+++ home.js
@@ -22,6 +22,7 @@ const nbLinks = [
 
 const navRight = document.getElementById('nav-right');
 
+// render navbar
 nbLinks.forEach(i => {
     const newLink = document.createElement('a');
     const li = document.createElement('li');
@@ -29,4 +30,27 @@ nbLinks.forEach(i => {
     newLink.setAttribute("href", i.href);
     li.appendChild(newLink);
     navRight.appendChild(li);
+});
+
+// render hidden part of navbar
+const hiddenMenu = document.querySelector('nav>ul');
+
+nbLinks.forEach(i => {
+    const newLink = document.createElement('a');
+    const li = document.createElement('li');
+    newLink.textContent = i.name;
+    newLink.setAttribute("href", i.href);
+    li.appendChild(newLink);
+    hiddenMenu.appendChild(li);
+});
+
+// hamburger menu functionality
+const hamburgerMenu = document.querySelector('#nav-right>img');
+
+hamburgerMenu.addEventListener('click', () => {
+    if ([... hiddenMenu.classList].includes("hidden")) {
+        hiddenMenu.classList.remove("hidden");
+        return;
+    }
+    hiddenMenu.classList.add("hidden");
 });
