9415de2b2872d39a13d019493c9608968d831b78
rjames187 <rory.james2021@gmail.com>
2022-08-16 17:48:09 -0500

    add images and image  data (not yet rendered)

---
+++ image-data.js
@@ -0,0 +1,96 @@
+const SPONSOR_DATA = [
+    {
+        category: "Reserve Champion ($5,000)",
+        items: [
+            {
+                name: "Gabe and Angela Kwentus and Family",
+            },
+            {
+                name: "Herschman Family Foundation",
+            },
+            {
+                name: "Schneider Wealth Management",
+                file: "schneider.png",
+                href: "https://www.schneider-wealth.com/",
+            }
+        ],
+    },
+    {
+        category: "Award of Excellence ($2,500)",
+        items: [
+            {
+                name: "Grubbs Family",
+            },
+            {
+                name: "Precision Eagle Pools and Remodeling",
+                file: "preceagle.png",
+                href: "https://precisioneaglepools.com/",
+            },
+            {
+                name: "Kay Family",
+            },
+            {
+                name: "Ride with Pride Inc.",
+                file: "rdwp.png",
+                href: "http://www.ridewithprideinc.com/",
+            }
+        ],
+    },
+    {
+        category: "Blue Ribbon ($1,000)",
+        items: [
+            {
+                name: "Cosmetic and Family Dentistry of North Texas",
+                file: "cafdnt.gif",
+                href: "https://dentistincolleyville.com/",
+            },
+            {
+                name: "Tomorrows Designs",
+                file: "td.png",
+                href: "https://tomorrowsdesigns.com/",
+            },
+            {
+                name: "Outlaw Equine Hospital Rehab and Center",
+                file: "ole.png",
+                href: "https://www.outlawequinevet.com/",
+            },
+            {
+                name: "Paul and Susan Straten",
+            },
+            {
+                name: "Wyred Audio/Video",
+                file: "wy.png",
+                href: "https://wyredav.com/"
+            },
+            {
+                name: "The Bruton Family",
+            },
+            {
+                name: "Al and Phyllis Zito",
+            },
+            {
+                name: "Stefanko Family",
+            }
+        ],
+    },
+    {
+        category: "Red Ribbon ($500)",
+        items: [
+            {
+                name: "Brett Zimmerman Investments",
+            },
+            {
+                name: "Pappa's and Co. CPA",
+            },
+            {
+                name: "Master Made Feed",
+            },
+            {
+                name: "Laura and Shawn McCaskill",
+            },
+            {
+                name: "First Financial Bank",
+            }
+        ],
+    }
+];
