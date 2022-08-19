581425158e4de12aab01ec988c66c9144b6ab55a
rjames187 <rory.james2021@gmail.com>
2022-08-19 15:01:41 -0500

    setup about page

---
+++ about.js
@@ -0,0 +1,26 @@
+import { NAVBAR_DATA } from './image-data.js';
+
+
+const navRight = document.getElementById('nav-right');
+
+// render navbar
+NAVBAR_DATA.forEach(i => {
+    const newLink = document.createElement('a');
+    const li = document.createElement('li');
+    newLink.textContent = i.name;
+    newLink.setAttribute("href", i.href);
+    li.appendChild(newLink);
+    navRight.appendChild(li);
+});
+
+// render hidden part of navbar
+const hiddenMenu = document.querySelector('nav>ul');
+
+NAVBAR_DATA.forEach(i => {
+    const newLink = document.createElement('a');
+    const li = document.createElement('li');
+    newLink.textContent = i.name;
+    newLink.setAttribute("href", i.href);
+    li.appendChild(newLink);
+    hiddenMenu.appendChild(li);
+});
